f58a03777f4a969fc2479c32f6e36055a78b8cfe
Lesly <leslyr562@gmail.com>
2021-12-16 17:45:14 -0600

    added comments and fixed date

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -6,8 +6,17 @@ const pizzaController = {
 
 // get all pizzas
 getAllPizza(req, res) {
-    Pizza.find({})
-      .then(dbPizzaData => res.json(dbPizzaData))
+  Pizza.find({})
+  .populate({
+    //To populate a field, just chain the .populate() method onto your query, passing in an object with the key path plus the value of the field you want populated.
+    path: 'comments',
+    //The minus sign - in front of the field indicates that we don't want it
+    select: '-__v'
+  })
+  .select('-__v')
+  //to sort in DESC order by the _id value.
+  .sort({ _id: -1 })
+  .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
         res.status(400).json(err);
@@ -15,22 +24,26 @@ getAllPizza(req, res) {
   },
 
   // get one pizza by id
-  getPizzaById({ params }, res) {
-    Pizza.findOne({ _id: params.id })
-      .then(dbPizzaData => {
-        // If no pizza is found, send 404
-        if (!dbPizzaData) {
-          res.status(404).json({ message: 'No pizza found with this id!' });
-          return;
-        }
-        res.json(dbPizzaData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
-      });
-  },
-
+  // get one pizza by id
+getPizzaById({ params }, res) {
+  Pizza.findOne({ _id: params.id })
+    .populate({
+      path: 'comments',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbPizzaData => {
+      if (!dbPizzaData) {
+        res.status(404).json({ message: 'No pizza found with this id!' });
+        return;
+      }
+      res.json(dbPizzaData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+},
   // createPizza POST
 createPizza({ body }, res) {
     Pizza.create(body)
